252b8d21abc4b061088b8479ddfffe1f8067458a
Pau Ametller <115103720+PauAmetller@users.noreply.github.com>
2024-05-31 17:50:08 +0200

    decals scene 2 not all

--- Assets/Scripts/DisapearWood/DisapearWoodMovement.cs
+++ Assets/Scripts/DisapearWood/DisapearWoodMovement.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 public class DisapearWoodMovement : MonoBehaviour
 {
     public GameObject wood;
-    public GameObject wall;
     
     public void DestroyWood()
     {
         if (wood != null)
         {
             Destroy(wood, 1.0f);
-        }
-        if (wall != null)
-        {
-            Destroy(wall, 1.0f);
         }
     }
 }
--- Assets/Scripts/DisapearWood/DisapearWoodRotateLever.cs
+++ Assets/Scripts/DisapearWood/DisapearWoodRotateLever.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*public enum Estado
-{
-    Stop,
-    PositiveR,
-    NegativeR
-}Lo comento porque sino peta y para saber que existe ya que se usa.*/
 
 public class DisapearWoodRotateLever : MonoBehaviour
 {
     private bool isRotated = false; //Dira si ya ha rotado o si esta en la posicion inicial
     public float TEspera = 1.0f;
-    private Estado needRotation = Estado.Stop;
+    //private Estado needRotation = Estado.Stop;
     private float targetRotation = 0f; //Inicialiaz la variable donde guardaremos la posicion objetivo
     private bool isRotating = false; // Controla si esta rotando para no hacer nada mas
     private float initialRotation;
@@ -48,7 +42,7 @@ public class DisapearWoodRotateLever : MonoBehaviour
                     
                 }
                 isRotating = false;
-                needRotation = Estado.Stop;
+                //needRotation = Estado.Stop;
                 rotationTimeElapsed = 0f;
                 if(oneRotation)
                 {
@@ -66,12 +60,12 @@ public class DisapearWoodRotateLever : MonoBehaviour
             initialRotation = transform.eulerAngles.x;
             if (isRotated)
             {
-                needRotation = Estado.NegativeR;
+                //needRotation = Estado.NegativeR;
                 targetRotation = initialRotation - 90f;
             }
             else
             {
-                needRotation = Estado.PositiveR;
+                //needRotation = Estado.PositiveR;
                 targetRotation = initialRotation + 90f;
             }
             isRotated = !isRotated;
